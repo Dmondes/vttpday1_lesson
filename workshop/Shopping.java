ac40e85e78db968296a79669393e38b59d82b485
Dmondes <desmondlimws@gmail.com>
2024-10-03 15:52:54 +0800

    completed workshop

--- workshop/Shopping.java
+++ workshop/Shopping.java
@@ -7,28 +7,44 @@ public class Shopping{
     public static void main(String[] args){
         Console cons = System.console();
         List<String> cart = new ArrayList<>();
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Welcome to your shopping cart");
-        String item = scanner.next();
-        switch (item.toLowerCase()){
+        while (true){
+        String theInput = cons.readLine("Welcome to your shopping cart\n ");
+        if (theInput.equalsIgnoreCase("stop")){
+            System.out.println("Checking out...");
+            break;
+        }
+        
+        Scanner scan = new Scanner(theInput);
+        switch (scan.next().toLowerCase()){
             case "list":
                 if (cart.size() == 0){
                     System.out.println("Cart is empty");
+                    break;
                 }
                 else{
                     for (int i = 0; i < cart.size(); i ++){
-                        System.out.printf("%d.%s\n", i+1, cart.get(i));
+                        System.out.printf("%d : %s\n", i+1, cart.get(i));
                     }
+                    break;
                 }
             case "add":
-                while (true){
-                    if (item == " "){
-                        break;
-                    }
+                while (scan.hasNext()){
+                    String item = scan.next();
                 cart.add(item);
+                System.out.printf("%s added to cart\n", item);
+
                 }
+                break;
             case "delete":
-                cart.remove(item);
+                int count = Integer.parseInt(scan.next());
+                if (count > cart.size() + 1){
+                    System.out.println("Incorrect item index\n");
+                }
+                else{
+                System.out.printf("%s removed from cart\n", cart.get(count - 1));
+                cart.remove(count - 1);}
         }
+        scan.close();
     }
+}
 }
